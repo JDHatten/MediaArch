856c96924e6ec63866f36704b29af225506d7f3a
JDHatten <GrahfAzura@gmail.com>
2024-08-21 16:28:12 -0700

    Refactoring, unsaved_settings

--- UserSettings.h
+++ UserSettings.h
@@ -12,33 +12,35 @@
 #include <QtSql/QSqlField>
 
 #include "MediaSpaces.h"
-#include "MediaViewer.h"
 
 class UserSettings
 {
-
-private:
-
-    int UserId = 0;
-    QSqlDatabase database;
-    QSqlRecord user_record;
-    
-    bool CreateNewUserSettingsRecord();
-    bool DeleteUserSettings();
-
 public:
 
     UserSettings(int user_id);
     ~UserSettings();
 
     void loadUserSettings();
-    bool saveUserSettings();// All Fields
-    bool saveUserSettings(QString field);
-    bool saveUserSettings(QList<QString> fields);
-
+    bool saveUserSettings();
+    //bool saveUserSettings(QString field);
+    //bool saveUserSettings(QStringList fields);
+    bool areSettingsUnsaved() const;
+    bool hasSettingChanged(QString field) const;
+    QVariant getUnsavedUserSetting(QString field) const;
     QVariant getUserSetting(QString field) const;
     bool setUserSetting(QString field, QVariant value, bool save_user_settings = false);
     void printSettings() const;
+
+private:
+
+    int user_id = 0;
+    QSqlDatabase database;
+    QSqlRecord user_record;
+    QVariantHash unsaved_settings;
+    QStringList fields_changed;
+    
+    bool CreateNewUserSettingsRecord();
+    bool DeleteUserSettings();
 
 };
 
