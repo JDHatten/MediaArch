105ff99466abaa4c674e7ea62f61cbb2600614c1
JDHatten <GrahfAzura@gmail.com>
2024-10-08 18:47:28 -0700

    Refactoring, Styling

--- UserMetaWidget.h
+++ UserMetaWidget.h
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef USERDATAWIDGET_H
-#define USERDATAWIDGET_H
+#ifndef USERMETAWIDGET_H
+#define USERMETAWIDGET_H
 
 #include <filesystem>
 
@@ -9,14 +9,14 @@
 #include <QMouseEvent>
 #include <QPushButton>
 //#include <QResizeEvent>
-//#include <QScrollArea>
+#include <QScrollArea>
 //#include <QScrollBar>
 #include <QString>
 #include <QTextEdit>
 #include <QTime>
 
-//#include <MediaInfoDLL.h>
 #include "MediaSpaces.h"
+#include "UserSettings.h"
 //#include "MetaDatabaseWorker.h"
 
 class UserMetaWidget : public QWidget
@@ -25,13 +25,13 @@ class UserMetaWidget : public QWidget
 
 public:
 
-    UserMetaWidget(int user_id, QWidget* parent = Q_NULLPTR);
+    UserMetaWidget(UserSettings* user_settings, QWidget* parent = Q_NULLPTR);
     ~UserMetaWidget();
 
     int getStarRating();
     QString getUserNotes();
     void setUserMeta(std::filesystem::path file_path, std::map<QString, QVariant>* user_meta);
-    void changeUserId(int user_id); // TODO
+    //void changeUserId(int user_id); // TODO
 
 signals:
 
@@ -49,6 +49,7 @@ private slots:
 
 private:
     int user_id = 0;
+    UserSettings* user_settings = nullptr;
     std::filesystem::path file_path;
     bool is_ready_to_use = false;
 
@@ -58,13 +59,16 @@ private:
     QWidget* date_last_opened_widget = nullptr;
     QLabel* date_last_opened_label = nullptr;
     QLabel* date_last_opened = nullptr;
+    QWidget* open_count_widget = nullptr;
+    QLabel* open_count_label = nullptr;
+    QLabel* open_count = nullptr;
     QTextEdit* user_file_notes = nullptr;
     QPushButton* edit_notes_button = nullptr;
 
     std::map<QString, QVariant>* user_meta;
 
-    int current_star_count = 0;
-    int current_star_count_highlighted = -1;
+    int current_star_count = -1;
+    int current_star_count_highlighted = 0;
 
     void UpdateUserMeta();
     void UpdateStarRating(int rating, bool save = false);
@@ -75,7 +79,6 @@ protected:
     void mouseMoveEvent(QMouseEvent* mouse_event) override;
     void mousePressEvent(QMouseEvent* mouse_event) override;
     //void mouseDoubleClickEvent(QMouseEvent* mouse_event) override;
-    //void paintEvent(QPaintEvent* paint_event) override;
 };
 
-#endif // USERDATAWIDGET_H
+#endif // USERMETAWIDGET_H
